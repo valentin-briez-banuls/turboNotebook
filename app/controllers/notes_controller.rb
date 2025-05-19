d9faaf34923689c217cad4707e456f10469673f7
Valentin BRIEZ-BANULS <valentin.briez-banuls@imerir.com>
2025-05-19 10:38:16 +0200

    all

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,64 @@
+class NotesController < ApplicationController
+  before_action :set_note, only: %i[ show edit update destroy ]
+
+  def index
+    @notes = Note.all
+  end
+
+  def show
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def edit
+  end
+
+  def create
+    @note = Note.new(note_params)
+
+    respond_to do |format|
+      if @note.save
+        format.turbo_stream
+        format.html { redirect_to @note, notice: "Note was successfully created." }
+        format.json { render :show, status: :created, location: @note }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @note.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @note.update(note_params)
+        format.turbo_stream
+        format.html { redirect_to @note, notice: "Note was successfully updated." }
+        format.json { render :show, status: :ok, location: @note }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @note.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @note.destroy
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to notes_path, status: :see_other, notice: "Note was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_note
+      @note = Note.find(params[:id])
+    end
+
+    def note_params
+      params.require(:note).permit(:title, :content)
+    end
+end
